7e06c73e80f8d72bb56c496cba0541472cf81067
NamLe0609 <lehoangnamtep@gmail.com>
2023-11-16 17:50:23 +0000

    Start extracting

--- jssb25.py
+++ jssb25.py
@@ -5,7 +5,7 @@ import re
 def cleanCategorical(df, categoricalData):
     for column in categoricalData:
         df[column] = df[column].str.casefold().str.strip()
-        df[column] = df[column].str.extract(r'([a-zA-Z0-9\s\-\/,&]+)')
+        df[column] = df[column].str.extract(r'([a-zA-Z0-9\s\-\/,&.]+)')
         df[column] = df[column].fillna('NA')
     return df
 
@@ -89,21 +89,21 @@ def cleanOS(df):
 
 def standardizeFeatures(row):
     sfMapping = {
-        r'anti[ -]?glare[ -]?(?:screen|coating)?|anti[ -]?(?:gla| glare|reflection)' : 'anti-glare',
-        r'(?:backlit|backlight)[ -]?(?:kb|kyb|keyboard)?': 'backlit_keyboard',
-        r'(?:nano|infinity)edge|(?:thin|narrow|ultra-narrow|micro-edge|nano[ -]?edge)[ -]?bezel[s]?|bezel[s]?': 'thin_bezel', #https://www.tomshardware.com/news/dell-infinityedge-oled-monitors,30854.html https://linustechtips.com/topic/1242078-what-the-hell-is-nanoedge-by-asus/
-        r'(?:active|support)[ -]?stylus|pen|stylus': 'stylus',
-        r'high definition audio|hd[ -]?audio': 'hd_audio',
-        r'fingerprint reader|fingerprint': 'fingerprint_reader',
-        r'speakers|stereo[ -]?[speakers]?': 'stereo_speakers',
+        r'anti-? ?glare|anti[ -]?(?:gla|reflection)' : 'anti-glare',
+        r'backlit|backlight': 'backlit_keyboard',
+        r'edge|thin|narrow|bezel': 'thin_bezel', #https://www.tomshardware.com/news/dell-infinityedge-oled-monitors,30854.html https://linustechtips.com/topic/1242078-what-the-hell-is-nanoedge-by-asus/
+        r'stylus|pen|stylus': 'stylus',
+        r'audio': 'hd_audio',
+        r'fingerprint': 'fingerprint_reader',
+        r'speakers|stereo': 'stereo_speakers',
         r'wifi & bluetooth': 'wifi_and_bluetooth',
-        r'(?:water|spill)[ -]?resistant|water[ -]?proof|dishwasher safe': 'water_resistant',
-        r'corning[ -]?gorilla[ -]?glass': 'corning_gorilla_glass',
-        r'[numeric]?[ -]?keypad': 'numeric_keypad',
-        r'chiclet[ -]?keyboard|[keyboard: ]?chiclet': 'chiclet_keyboard',
-        r'touch[ -]?screen[ -]?[laptop]?': 'touch-screen',
+        r'resistant|water|dishwasher': 'water_resistant',
+        r'gorilla': 'corning_gorilla_glass',
+        r'keypad': 'numeric_keypad',
+        r'chiclet': 'chiclet_keyboard',
+        r'touch[ -]?screen': 'touch-screen',
         r'multi[ -]?touch': 'multi-touch',
-        r'[amazon]?[ -]?alexa': 'alexa',
+        r'alexa': 'alexa',
         r'light and compact|narrow|space saving|portable': 'lightweight',
         r'information not available|and play on a fast|work|create|high quality|built for entertainment|premium business-class notebook': 'NA',
     }
@@ -130,6 +130,7 @@ def standardizeFeatures(row):
 def cleanSpecialFeatures(df):
     df['special_features'] = df['special_features'].str.split(',')
     df['special_features'] = df['special_features'].apply(standardizeFeatures)
+    #df = df.explode('special_features')
     return df
 
 def extractGPU(df):
@@ -144,35 +145,42 @@ def standardizeGPU(row):
     gpuMappingClean = {
         
     }
+    gpuExtract = {
+        r'(?P<gpu_series>nvidia_quadro)(?: nvidia_rtx)? ?(?P<gpu_model>[atpk]\d{3,4}m?|\d{4})', # Nvidia quatro
+        r'(?P<gpu_series>nvidia_rtx)\s?(?P<gpu_model>\d{4}(?:_ti| ti)?|[at]\d{3,4})', # Nvidia rtx
+        r'(?P<gpu_series>nvidia_gtx)\s?(?P<gpu_model>(\d{4})(?:[_ ]?(ti))?|[at]?\d{3,4}m?)', # Nvidia gtx
+        r'(nvidia_mx)\s?(\d{3})', # Nvidia mx
+    }
     
-    for regex, gpu in gpuMappingClean.items():
-        if re.search(regex, row):
-            return gpu
+    for regex in gpuExtract:
+        if match := re.search(regex, row):
+            return match.group('gpu_series') + ' ' + match.group('gpu_model').replace(' ', '_')
     return row
 
 def cleanGPU(df):
     gpuMapping = {
-        r'intel (?:iris xe|integrated) graphics|intel iris plus|integrated iris xe graphics|intel iris': 'intel_iris',
+        r'intel (?:iris xe|integrated) graphics|intel iris plus|integrated iris xe graphics|intel iris|iris xe': 'intel_iris',
         r'mediatek integrated|mediatek graphics': 'mediatek',
         r'nvidia geforce[r]?|geforce': 'nvidia',
         r'nvidia (?:trx|rtx)|nvidia intel rtx|\brtx\b': 'nvidia_rtx',
         r'nvidia quadro rtx': 'nvidia_quartro_rtx',
         r'nvidia quadro|quadro': 'nvidia_quadro',
         r'nvidia gt[x]?': 'nvidia_gtx',
-        r'nvidia mx': 'nivida_mx ',
+        r'nvidia mx': 'nvidia_mx ',
         r'intel hd graphics|intel hd': 'intel_hd',
         r'intel uhd graphics|uhd graphics': 'intel_uhd',
         r'amd radeon graphics|amd radeon': 'amd_radeon',
         r'intel graphics integrated|integrated intel graphic[s]?': 'intel',
         r'amd integrated graphics': 'amd',
         r'apple integrated': 'apple',
-        r'integrated[ _]?graphics|embedded|dedicated|intergrated|integreted': 'integrated',
+        r'integrated[ _]?graphics|embedded|dedicated|intergrated|integreted': 'dedicated',
     }
     
     for regex, gpu in gpuMapping.items():
         df['graphics_coprocessor'] = df['graphics_coprocessor'].str.replace(regex, gpu, regex=True)
         
     #df['graphics_coprocessor'] = df['graphics_coprocessor'].str.replace(' ', '_', regex=True)
+    df['graphics_coprocessor'] = df['graphics_coprocessor'].apply(standardizeGPU)
     
     return df
 
@@ -200,19 +208,19 @@ def main():
     # and to put those into numerical feature list instead of categorical
     numericalData = ['harddisk', 'ram', 'screen_size', 'cpu_speed', 'rating', 'price']
     df = cleanAllNum(df, numericalData)
-    """ 
-    df = cleanHDD(df) # Multiply TB values (less than 8) by 1024 to make it GB
+    
+    """ df = cleanHDD(df) # Multiply TB values (less than 8) by 1024 to make it GB
     df = cleanCPUSpeed(df) # Divide MHz values (more than 10) by 1000 to make it GHz
     df = cleanRam(df) # Ram is only integer amount. Round in case value is not integer
     df = cleanColor(df) # Clean color to remove non-standard values
     df = cleanOS(df) # Clean OS by simplifying it to OS type, and version
     df = cleanSpecialFeatures(df) # Clean special features by standardising features which are the same
-    df = df.drop_duplicates(ignore_index=True, keep='first') # Drop rows which are exact duplicates
-     """
+    df = df.drop_duplicates(ignore_index=True, keep='first') # Drop rows which are exact duplicates """
+    
     df = extractGPU(df)
     df = cleanGPU(df)
     print(df['graphics_coprocessor'].value_counts())
-    #print(df['graphics'].value_counts())
+    
     df = df.rename(columns={
         "harddisk": "harddisk_gb", 
         "ram": "ram_gb", 
